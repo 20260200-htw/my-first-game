5529df7fd39cc9944d0c7081e691e207be4cb49c
hantaeung <20260200@vision.hoseo.edu>
2026-06-05 00:51:15 +0900

    1-1 임시 스토리 작성

--- 기말(파일 분리 버전, 이걸로 개발)/game/data/story/story_data_1_1.py
+++ 기말(파일 분리 버전, 이걸로 개발)/game/data/story/story_data_1_1.py
@@ -10,35 +10,412 @@ CHAPTER = {
             "image": "",
             "dialogue": [
                 {
-                    "characters": [],
+                    "characters": [
+                    ],
                     "background": "",
-                    "affiliation": "???",
+                    "affiliation": "이야기의 주인공",
                     "speaker": "주인공",
-                    "text": "눈을 떠보니... 낯선 풍경이다.",
+                    "text": "(꿈이라고 믿고 싶지만... 이건 도저히 부정할 수가 없는 비주얼이네)",
                 },
                 {
-                    "characters": [],
-                    "affiliation": "???",
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
                     "speaker": "주인공",
-                    "text": "여기가 중계자가 말하던 그 세계인가.",
+                    "text": "(보통 이세계물은 수도 왕국에 소환되던데... 여기도 비슷한 곳인건가?)",
                 },
                 {
-                    "characters": [],
-                    "affiliation": "행인",
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
                     "speaker": "???",
-                    "text": "어이, 거기 너! 멍하니 서 있지 말고 비켜!",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "아, 아 아. 들리시나요?",
                 },
                 {
-                    "characters": [],
-                    "affiliation": "???",
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
                     "speaker": "주인공",
-                    "text": "(사람이다. 말도 통하는군.)",
+                    "text": "뭐, 뭐야?",
                 },
                 {
-                    "characters": [],
-                    "affiliation": "???",
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "반응을 보니 들리시는 것 같네요!",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
                     "speaker": "주인공",
-                    "text": "일단... 이 세계가 어떤 곳인지부터 알아봐야겠다.",
+                    "text": "아, '닿는다' 라는 게 이런 거군요... 놀랬네.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "아, 이건 '닿은 것'이 아니예요. 제가 일방적으로 조언하는 거죠.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "그게 그거 아닌ㄱ...",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "잠시만요! 아, 말 끊어서 죄송해요. 육성으로 말하지 않으셔도 저는 당신의 목소리를 들을 수 있어요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(이렇게요?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "네.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(당연한 질문이겠지만, 중계자... 씨가 하시는 말은 제게만 들리는 거죠?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "네. 그것도 맞아요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "제가 가진 권한상 지금 당신에게 해드릴 수 있는 말은 많지 않아요. 그러니까 중요한 것들만 말씀드릴게요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "일단 판타지아에서 모험을 떠나려면 '모험가 자격'을 취득해야 해요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "없다고 해서 모험을 떠나는 것이 불가능한 것은 아니지만, 이를 취득하지 않으면 많은 제약이 따를 거예요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "제가 전송해 드린 장소는 '중앙 왕국'이라고 불리는 곳이에요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "그리고 이곳에는 말씀드린 '모험가 자격'을 취득할 수 있는 '모험가 길드'의 본 건물이 존재하고요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(일단 그곳으로 가면 되는 건가요?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "네. 하지만 지금 바로는 안 될 거예요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(역시 모험가가 되려면 최소한의 강함은 필요한가 보네요.)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "아, 아니요. 모험가는 누구나 될 수 있답니다.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(그럼 왜...?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "'모험가 자격'은 등급이 존재해요. 그리고 등급은 해당 모험가의 수준에 비례한답니다.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(처음 정해진 등급을 올리는 것이 불가능한 건가요?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "등급을 올리는 건 가능해요. 하지만, 동부나 서부로 향하는 통행권을 취득하기 위해선 최소 4급 모험가 자격이 필요해요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "처음으로 취득하게 되는 '모험가 자격'의 등급은 '모험가 심사'의 결과에 따라 정해진답니다.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "'모험가 심사'는 5급부터 3급까지 신입 모험가들의 등급이 정해져요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "일반적인 5급 모험가가 4급이 되기 위해서는 최소 수 년의 시간이 필요해요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "'모험가 심사'에서 좋은 성적을 거두어 최대한 높은 등급으로 시작하는 것이 훨씬 유리해요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(그 '모험가 심사'라는 건 무엇을 심사하나요? 마물 토벌?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "듣기로는 과거에는 다양한 심사가 존재했었다고 해요. 하지만 최근 100년 동안은 신입 모험가들끼리의 결투 심사만 진행됐다고 하더라구요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(다른 모험가들과 싸워서 승리할 정도의 실력은 길러야겠네요.)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "네, 그런 셈이죠!",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(...)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "그래도 너무 걱정은 안 하셔도 돼요! 신입 모험가들의 실력에 따라 그룹을 만들어 결투하니 비슷한 실력을 가진 사람들과 싸우게 될 거예요!",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(그나마 위안이 되는 말이네요...)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "당신의 새로운 몸은 제가 판타지안과 똑같이 만들었으니 마력을 다루는 것이 그렇게 어렵지는 않으실 거예요!",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(판타지안?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "판타지아에 사는 모든 인족들을 통틀어 부르는 단어랍니다. 판타지아에선 흔히 사용되는 표현이 아니니 크게 신경쓰지 않으셔도 돼요.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(판타지아에서 흔히 사용되는 게 아니면 어디서 쓰이는데요?)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "■■, ■■■■, ■■ ■■ ■■■ ■■, 그리고 ■■■■ ■■■...랑 ■■■ ■■■■■■■■■ ■■",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "중계자",
+                    "speaker": "???",
+                    "sound": "assets/BC/broadcaster.mp3",
+                    "text": "어? 아, 아니 그 죄송해요! 갑자기 연결ㅇ.이.ㅇ.이..ㅇ.ㄲ.ㅡㅎㄴ..ㄱㅕ.ㅕ.",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "...;",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(중계자라는 여자... 아니 그전에 성별이 존재하기는 하려나. 아무튼 어딘가 좀 어리버리한 사람이네.)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(일단 '모험가 심사'라는 걸 치뤄야 하니, 먼저 레벨업을 하는 게 맞는 방향이겠지.)",
+                },
+                {
+                    "characters": [
+                        {"sprite": "assets/MC/main_character_B.png", "x": 0.50, "y": 1, "scale": 0.75},
+                    ],
+                    "affiliation": "이야기의 주인공",
+                    "speaker": "주인공",
+                    "text": "(이곳은 안전한 구역 같으니 좀 나가서 마물이 있는 곳을 찾아봐야 겠다.)",
+                },
+                {
+                    "characters": [
+                    
+                    ],
+                    "text": "그렇게 나는 왕국을 벗어나 성벽 외곽을 둘러보기로 했다.",
                 },
             ],
         },
